09e8e0a115e6d0210224d94d9dffa06a08894e57
andrey_fedorov <andrjuxa97@mail.ru>
2020-10-25 16:31:47 +0400

    commit 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,50 @@
 import { Component } from '@angular/core';
+import {AuthService} from "./core/auth/auth.service";
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  template: `
+    <header>
+      <nav class="navbar navbar-expand-md navbar-dark fixed-bottom bg-danger">
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+              <a *ngIf="isLogged()" class="nav-link" routerLink="/">Account<span class="sr-only">(current)</span></a>
+            </li>
+            <li *ngIf="isLogged()" class="nav-item">
+              <a class="nav-link" routerLink="/contacts">Contacts</a>
+            </li>
+            <li *ngIf="!isLogged()" class="nav-item">
+              <a class="nav-link" routerLink="/login">Login</a>
+            </li>
+            <li *ngIf="isLogged()" class="nav-item">
+              <a class="nav-link btn" (click)="logout()">Logout</a>
+            </li>
+          </ul>
+        </div>
+        <a *ngIf="isLogged()" class="navbar-text" routerLink="/">
+          {{ getLogin() }}
+        </a>
+      </nav>
+    </header>
+    <ngx-bootstrap-spinner loadingText="Loading..."></ngx-bootstrap-spinner>
+    <router-outlet></router-outlet>
+`
 })
 export class AppComponent {
-  title = 'personal-account';
+
+  constructor(private authService: AuthService) {}
+
+  logout(): void {
+    this.authService.logout();
+  }
+
+  isLogged(): boolean {
+    return this.authService.isLoggedIn()
+  }
+
+  getLogin() {
+    return this.authService.getUser().getLogin();
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from './router/config/app-routing.module';
 import { AppComponent } from './app.component';
+import {SharedModule} from "./shared/shared.module";
+import {GuardsModule} from "./router/guards.module";
+import {ComponentsModule} from "./modules/components.module";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    GuardsModule,
+    SharedModule,
+    ComponentsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
